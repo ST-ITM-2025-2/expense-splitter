3c4cdfdc6412de2c81b81ba535649525b902dacb
JUNU <m.l02n23@gmail.com>
2025-10-17 23:20:26 +0900

    add: add file to test Splitter.computeBalances

---
+++ src/test/java/itm/oss/splitter/SplitterTest.java
@@ -0,0 +1,19 @@
+package itm.oss.splitter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SplitterTest {
+    private final Splitter splitter = new Splitter();
+    @Test
+    @DisplayName("Test for compute balance")
+    void testComputeBalance() {
+        // TODO: Implement Test case at least one happy-path
+
+        // TODO: Implement Test case at least one edge-case test
+    }
+}
