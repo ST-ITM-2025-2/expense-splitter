df0773b01d2855fe6436a4c9c25e9a077dae5ac3
Matias Tarvainen <tarvainen.matias@gmail.com>
2025-10-17 14:42:23 +0900

    Issue one completed
    The issue one reads a .csv file into an expences array. this can now be used to mock our own issue number 7

--- src/main/java/itm/oss/splitter/ExpenseStore.java
+++ src/main/java/itm/oss/splitter/ExpenseStore.java
@@ -1,15 +1,52 @@
 package itm.oss.splitter;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class ExpenseStore {
 
-  public ArrayList<Expense> load(String path) throws IOException {
-    // TODO (Issue 1): parse CSV file into Expense list.
-    // Format: date,payer,amount,currency,participants,category,notes
-    // participants are semicolon-separated.
-    throw new UnsupportedOperationException("load() not implemented yet");
+    public ArrayList<Expense> load(String path) throws IOException {
+        // TODO (Issue 1): parse CSV file into Expense list.
+        // Format: date,payer,amount,currency,participants,category,notes
+        // participants are semicolon-separated.
+        String line = "";
+        ArrayList<Expense> expenses = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            while ((line = br.readLine()) != null) {
+                String date = "";
+                String payer = "";
+                BigDecimal amount = null;
+                String currency = "";
+                String category = "";
+                String notes = "";
+
+                String[] values = line.split(",");
+                if (Objects.equals(values[0].trim(), "date")){
+                    continue;
+                }
+
+                date = values[0];
+                payer = values[1];
+                amount = new BigDecimal(values[2]);
+                currency = values[3];
+                ArrayList<String> participants = new ArrayList<>(Arrays.asList(values[4].split(";")));
+                category = values[5];
+                notes = values[6];
+
+                Expense expense = new Expense(date, payer, amount, currency, participants, category, notes);
+                expenses.add(expense);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return expenses;
   }
 
   public void append(String path, Expense e) throws IOException {
--- src/main/java/itm/oss/splitter/Reports.java
+++ src/main/java/itm/oss/splitter/Reports.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class Reports {
 
-  public SimpleMap totalsByCategory(ArrayList<Expense> xs) {
-    // TODO (Issue 6): sum amounts per category
-    throw new UnsupportedOperationException("totalsByCategory() not implemented yet");
-  }
+    public SimpleMap totalsByCategory(ArrayList<Expense> xs) {
+        // TODO (Issue 6): sum amounts per category
+        throw new UnsupportedOperationException("totalsByCategory() not implemented yet");
+    }
 
-  public SimplePersonSummaryMap perPerson(ArrayList<Expense> xs) {
-    // TODO (Issue 7): compute paidTotal, owedTotal, net per person
-    throw new UnsupportedOperationException("perPerson() not implemented yet");
-  }
+    public SimplePersonSummaryMap perPerson(ArrayList<Expense> xs) {
+        // TODO (Issue 7): compute paidTotal, owedTotal, net per person
+        throw new UnsupportedOperationException("perPerson() not implemented yet");
+    }
 }
