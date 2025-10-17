25c4df2814d0c57897743a66797e28ee4cf5d51c
Matias Tarvainen <tarvainen.matias@gmail.com>
2025-10-17 17:18:37 +0900

    Issue  7 solve
    Made the paid, owed, and net values. Had to change the personSummary file. Not sure if this was allowed for the task but looks better than having hashmaps in the file. A backup branch was made for the single file solution.

--- src/main/java/itm/oss/splitter/Reports.java
+++ src/main/java/itm/oss/splitter/Reports.java
@@ -1,5 +1,7 @@
 package itm.oss.splitter;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 public class Reports {
@@ -10,7 +12,30 @@ public class Reports {
     }
 
     public SimplePersonSummaryMap perPerson(ArrayList<Expense> xs) {
-        // TODO (Issue 7): compute paidTotal, owedTotal, net per person
-        throw new UnsupportedOperationException("perPerson() not implemented yet");
+        SimplePersonSummaryMap map = new SimplePersonSummaryMap();
+
+        for (Expense x : xs) {
+            int count = x.getParticipants().size();
+            if (count == 0) continue;
+
+            BigDecimal owedPerPerson = x.getAmount()
+                    .divide(new BigDecimal(count), 2, RoundingMode.HALF_UP);
+
+            for (String name : x.getParticipants()) {
+                if (map.get(name) == null) {
+                    map.put(name, new PersonSummary(BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO ));
+                }
+                PersonSummary ps = map.get(name);
+
+                if (name.equals(x.getPayer())) {
+                    ps.addPaid(x.getAmount());
+                }
+
+                ps.addOwed(owedPerPerson);
+            }
+        }
+
+        return map;
     }
+
 }
