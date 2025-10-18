5e7ec7c39adfaff55b0c3f3e4620c3c13723feb2
JUNU <m.l02n23@gmail.com>
2025-10-18 13:52:16 +0900

    feat: add two edge case

--- src/test/java/itm/oss/splitter/SplitterTest.java
+++ src/test/java/itm/oss/splitter/SplitterTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 class SplitterTest {
@@ -15,13 +16,15 @@ class SplitterTest {
     @Test
     @DisplayName("Test for compute balance")
     void testComputeBalance() {
-        ArrayList<Expense> expenses = new ArrayList<Expense>();
+        ArrayList<Expense> expensesHappyCase = new ArrayList<Expense>();
 
         ArrayList<String> names = new ArrayList<>();
         names.add("Alice");
         names.add("Bob");
         names.add("Cara");
 
+
+        // TODO: Implement Test case at least one happy-path
         Expense expense01 = new Expense("2025-10-01",
                 "Alice", BigDecimal.valueOf(70.00),
                 "USD",names,"Food","Pizza night");
@@ -35,15 +38,80 @@ class SplitterTest {
                 "Cara", BigDecimal.valueOf(90.00),
                 "USD",names,"Groceries","Market");
 
-        expenses.add(expense01);
-        expenses.add(expense02);
-        expenses.add(expense03);
-
-        // TODO: Implement Test case at least one happy-path
-        Balance result = splitter.computeBalances(expenses);
+        expensesHappyCase.add(expense01);
+        expensesHappyCase.add(expense02);
+        expensesHappyCase.add(expense03);
+        Balance resultHappyCase = splitter.computeBalances(expensesHappyCase);
+        System.out.println("\nTest for Happy Case : Each participant's net]\n");
         for(String name : names) {
-            System.out.println(name + " : " + result.getAmount(name));
+            System.out.println(name + " : " + resultHappyCase.getAmount(name));
         }
+        assertEquals(resultHappyCase.getAmount("Alice"), BigDecimal.valueOf(-23.34));
+        assertEquals(resultHappyCase.getAmount("Bob"), BigDecimal.valueOf(26.67));
+        assertEquals(resultHappyCase.getAmount("Cara"), BigDecimal.valueOf(-3.33));
+
         // TODO: Implement Test case at least one edge-case test
+
+        // 1. Case of the amount is negative -> pass that expense
+
+        ArrayList<Expense> edgeCaseNegative = new ArrayList<Expense>();
+
+        Expense expense04 = new Expense("2025-10-01",
+                "Alice", BigDecimal.valueOf(-100.00),
+                "USD",names,"Food","Pizza night");
+
+
+        Expense expense05 = new Expense("2025-10-02",
+                "Bob", BigDecimal.valueOf(80.00),
+                "USD",names,"Transport","Taxi + subway");
+
+        Expense expense06 = new Expense("2025-10-03",
+                "Cara", BigDecimal.valueOf(90.00),
+                "USD",names,"Groceries","Market");
+
+        edgeCaseNegative.add(expense04);
+        edgeCaseNegative.add(expense05);
+        edgeCaseNegative.add(expense06);
+
+        Balance resultEdgeNegative = splitter.computeBalances(edgeCaseNegative);
+        System.out.println("\nTest for Edge Case : negative amount - Each participant's net]\n");
+        for(String name : resultEdgeNegative.getNames()) {
+            System.out.println(name + " : " + resultEdgeNegative.getAmount(name));
+        }
+        assertEquals(resultEdgeNegative.getAmount("Alice"), BigDecimal.valueOf(-56.67));
+        assertEquals(resultEdgeNegative.getAmount("Bob"), BigDecimal.valueOf(23.34));
+        assertEquals(resultEdgeNegative.getAmount("Cara"), BigDecimal.valueOf(33.33));
+
+        // 2. Case of single participant
+
+        // only Alice pays all
+        names.remove("Bob");
+        names.remove("Cara");
+
+        ArrayList<Expense> EdgeCaseSingle = new ArrayList<Expense>();
+        Expense expense07 = new Expense("2025-10-01",
+                "Alice", BigDecimal.valueOf(60.00),
+                "USD",names,"Food","Pizza night");
+
+
+        Expense expense08 = new Expense("2025-10-02",
+                "Bob", BigDecimal.valueOf(120.00),
+                "USD",names,"Transport","Taxi + subway");
+
+        Expense expense09 = new Expense("2025-10-03",
+                "Cara", BigDecimal.valueOf(90.00),
+                "USD",names,"Groceries","Market");
+        EdgeCaseSingle.add(expense07);
+        EdgeCaseSingle.add(expense08);
+        EdgeCaseSingle.add(expense09);
+        Balance resultEdgeSingle = splitter.computeBalances(EdgeCaseSingle);
+        System.out.println("\n[Test for Edge Case : Single participants - Each participant's net]\n");
+
+        for(String name : resultEdgeSingle.getNames()) {
+            System.out.println(name + " : " + resultEdgeSingle.getAmount(name));
+        }
+        assertEquals(resultEdgeSingle.getAmount("Alice").setScale(2, RoundingMode.HALF_EVEN), BigDecimal.valueOf(-210.00).setScale(2, RoundingMode.HALF_EVEN));
+        assertEquals(resultEdgeSingle.getAmount("Bob").setScale(2, RoundingMode.HALF_EVEN), BigDecimal.valueOf(120.00).setScale(2, RoundingMode.HALF_EVEN));
+        assertEquals(resultEdgeSingle.getAmount("Cara").setScale(2, RoundingMode.HALF_EVEN), BigDecimal.valueOf(90.00).setScale(2, RoundingMode.HALF_EVEN));
     }
 }
