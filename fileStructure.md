70e74f51a315f7dc6dd2402117e9d685425484b9
Hong Junsoo <powerofjun@naver.com>
2025-10-19 04:23:57 +0900

    Issue9 task completed

---
+++ fileStructure.md
@@ -0,0 +1,9 @@
+Directory of Repository- Expense-splitter 
+1.	Data
+-	expenses.sample.csv
+
+2.	Docs
+-	cli-usage.md
+
+3.	Src/main/java/itm/oss/splitter
+-	App.java~ Splitter.java
--- src/main/java/itm/oss/splitter/Exporter.java
+++ src/main/java/itm/oss/splitter/Exporter.java
@@ -1,12 +1,30 @@
 package itm.oss.splitter;
 
 import java.io.*;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 public class Exporter {
 
   public static void writePaymentsCsv(String path, ArrayList<Payment> pays) throws IOException {
     // TODO (Issue 9): write CSV with header: from,to,amount
-    throw new UnsupportedOperationException("writePaymentsCsv() not implemented yet");
+
+    FileWriter writer = new FileWriter(path);
+
+    // CSV header
+    writer.write("from,to,amount\n");
+
+    // Write each payment line
+
+    for(int i = 0; i < pays.size(); i++) { // this can be expressed as (Payment p : pays) also... feedback? >.<
+      Payment p = pays.get(i);
+      String from = p.getFrom();
+      String to = p.getTo();
+      String amount = p.getAmount().setScale(2, RoundingMode.HALF_EVEN).toString();
+      writer.write(from + "," + to + "," + amount + "\n");
+    }
+
+    writer.close();
   }
+  
 }
