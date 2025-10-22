b717abcc469be040038e3f9941234cd4a78ad441
Minhee Jeong <jungminhee0730@naver.com>
2025-10-22 18:57:54 +0900

    implemented load function in ExpenseStore class

--- src/main/java/itm/oss/splitter/ExpenseStore.java
+++ src/main/java/itm/oss/splitter/ExpenseStore.java
@@ -1,7 +1,10 @@
 package itm.oss.splitter;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.nio.file.Paths;
 
 public class ExpenseStore {
 
@@ -9,7 +12,51 @@ public class ExpenseStore {
     // TODO (Issue 1): parse CSV file into Expense list.
     // Format: date,payer,amount,currency,participants,category,notes
     // participants are semicolon-separated.
-    throw new UnsupportedOperationException("load() not implemented yet");
+
+    // path = "data/expenses.csv"
+    ArrayList<Expense> returnList = new ArrayList<>() ; // the list that we will return
+
+    try (Scanner scanner = new Scanner(Paths.get(path))) {
+      if(scanner.hasNextLine()){ // because the first row of the csv file is headers, so we have to get rid of it 
+        scanner.nextLine(); // read one line and throw it (do not store it anywhere)
+      }
+      // we read the file until all lines have been read
+      int row_num = 1;
+      while (scanner.hasNextLine()) {
+        // we read one line
+        String expenseString = scanner.nextLine();
+        String[] expenseArray = expenseString.split(",");
+
+        if(expenseArray.length != 7){
+          System.out.println("Skipping row "+row_num+": some elements are missing") ;
+          continue ;
+        }
+
+        String date = expenseArray[0];
+        String payer = expenseArray[1];
+        BigDecimal amount = new BigDecimal(expenseArray[2]);
+        String currency = expenseArray[3];
+
+        ArrayList<String> participants = new ArrayList<>(); // expenseArray[4]
+        String[] participantArray = expenseArray[4].split(";");
+        for(String participant : participantArray){
+          participants.add(participant);
+        }
+
+        String category = expenseArray[5];
+        String notes = expenseArray[6];
+
+        Expense e = new Expense(date, payer, amount, currency, participants, category, notes);
+        returnList.add(e);
+        row_num++ ;
+
+      }
+
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+    // throw new UnsupportedOperationException("load() not implemented yet");
+    return returnList ;
   }
 
   public void append(String path, Expense e) throws IOException {
