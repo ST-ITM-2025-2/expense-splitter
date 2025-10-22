b13d803b9e484b5ef4c8d3a4bad37172bd672885
Lee, Jiseop <ljseop1030@gmail.com>
2025-10-22 21:51:30 +0900

    Add test code

--- src/main/java/itm/oss/splitter/ExpenseStore.java
+++ src/main/java/itm/oss/splitter/ExpenseStore.java
@@ -5,7 +5,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List; // List import 추가
 
 public class ExpenseStore {
 
