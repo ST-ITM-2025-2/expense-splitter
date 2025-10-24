60aea570dbf88bdeea1d76676aeba2d9a0d33aa3
Seong-bin LEE <yeonlimee@gmail.com>
2025-10-24 11:24:38 +0900

    Exception controll added

--- src/main/java/itm/oss/splitter/Exporter.java
+++ src/main/java/itm/oss/splitter/Exporter.java
@@ -9,22 +9,27 @@ public class Exporter {
   public static void writePaymentsCsv(String path, ArrayList<Payment> pays) throws IOException {
     // TODO (Issue 9): write CSV with header: from,to,amount
 
-    FileWriter writer = new FileWriter(path);
+    File file = new File(path);
 
-    // CSV header
-    writer.write("from,to,amount\n");
+    // try-with-resources로 자동 close
+    try (FileWriter writer = new FileWriter(file)) {
 
-    // Write each payment line
+      // CSV header
+      writer.write("from,to,amount");
 
-    for(int i = 0; i < pays.size(); i++) { 
-      Payment p = pays.get(i);
-      String from = p.getFrom();
-      String to = p.getTo();
-      String amount = p.getAmount().setScale(2, RoundingMode.HALF_EVEN).toString();
-      writer.write(from + "," + to + "," + amount + "\n");
+      // 리스트가 비어 있지 않을 때만 개행 및 내용 작성
+      if (pays != null && !pays.isEmpty()) {
+        writer.write("\n");
+
+        for (int i = 0; i < pays.size(); i++) {
+          Payment p = pays.get(i);
+          String from = p.getFrom();
+          String to = p.getTo();
+          String amount = p.getAmount().setScale(2, RoundingMode.HALF_EVEN).toString();
+          writer.write(from + "," + to + "," + amount + "\n");
+        }
+      }
     }
-
-    writer.close();
   }
-  
+
 }
