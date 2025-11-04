2ec6e8339d3a50fd2c42ef46175c00c16035ae30
Kimhyewon0621 <mykhw123@naver.com>
2025-11-04 19:00:57 +0900

    Update src/test/java/itm/oss/splitter/AppTest.java
    Co-authored-by: Seong-bin LEE <yeonlimee@gmail.com>

--- src/test/java/itm/oss/splitter/AppTest.java
+++ src/test/java/itm/oss/splitter/AppTest.java
@@ -14,9 +14,9 @@ import java.io.ByteArrayInputStream;
 public class AppTest {
 
     // JUnit 5 기본 구조
-    @Test
-    void testApp_Placeholder() {
-        assertTrue(true); 
+    @Test  
+    void testAppInitialization() {  
+        assertTrue(true);  
     }
 
     private final InputStream originalIn = System.in;
